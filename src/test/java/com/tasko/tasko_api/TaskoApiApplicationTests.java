da9005fbf35c45dcde66d6e283ae0e58ec23b64d
al3xandr3-bruno <brunoalexdias15@gmail.com>
2025-08-26 15:32:17 -0300

    🎉 New project for developing habits

---
+++ src/test/java/com/tasko/tasko_api/TaskoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tasko.tasko_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
