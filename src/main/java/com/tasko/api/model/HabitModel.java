121023af6f9103ebb3bd0cf62ad971addf81d3b3
al3xandr3-bruno <brunoalexdias15@gmail.com>
2025-08-28 14:16:52 -0300

    🔨 Creating representaion models for entities

---
+++ src/main/java/com/tasko/api/model/HabitModel.java
@@ -0,0 +1,9 @@
+package com.tasko.api.model;
+
+import java.util.Calendar;
+
+public class HabitModel {
+    private String name;
+    private String description;
+    private Calendar lastTimeFullfilled;
+}
---
+++ src/main/java/com/tasko/api/model/UserModel.java
@@ -0,0 +1,11 @@
+package com.tasko.api.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserModel {
+    private String name;
+    private Double points;
+}
